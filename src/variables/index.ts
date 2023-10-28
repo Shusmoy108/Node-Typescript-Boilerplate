7fc19686ca38f565afebf04d17bf0c88fa529f30
Chowdhury <sc26s@login.missouristate.edu>
2023-10-27 19:34:46 -0500

    typescript node js MVC boiler plate  completed

--- src/variables/index.ts
+++ src/variables/index.ts
@@ -4,16 +4,9 @@ dotenv.config();
 const env = process.env.APP_ENV || 'local';
 const appPort = process.env.PORT;
 const mongoUrl = process.env.MONGO_DB_URI;
-const api_key = process.env.MAILGUN_API_KEY;
-const domain = process.env.MAILGUN_DOMAIN;
-const email = process.env.EMAIL;
-const pass = process.env.PASS;
+
 export const Variables = {
   appPort,
   env,
   mongoUrl,
-  api_key,
-  domain,
-  email,
-  pass,
 };
